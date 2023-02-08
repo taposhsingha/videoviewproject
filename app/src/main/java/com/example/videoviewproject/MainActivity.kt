3040668fdbc0eebe39e453f5ca776ffe59799bc0
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2023-02-08 16:33:21 +0600

    files added

---
+++ app/src/main/java/com/example/videoviewproject/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.videoviewproject
+
+import android.net.Uri
+import android.net.Uri.parse
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import java.net.URI
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+
+        val uri: Uri = parse("android.resource://"+packageName+"/"+R.raw.samples)
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
+    }
+}
